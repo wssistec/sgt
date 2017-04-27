679a8ac4182bf03783670683ee290bfbc146f6e3
wssistec <wssistec@hotmail.com>
2017-04-27 09:13:29 -0300

    publish 01

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" >
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Bitnami: Open Source. Simplified</title>
+    <link href="//fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet" type="text/css">
+    <link href="./bitnami.css" media="screen" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body> 
+    <div id="container" class="container-virtual-applicances">
+      <div id="dashboard-content" class="launch-page">
+        <div id="dashboard-slogan" class="launch-slogan">
+          <h1>Congratulations!</h1>
+          <br/>
+          <table class="virtual-appliances-logo-block">
+            <tr>
+              <td>
+                 <img src="img/meanstack.png"/>
+              </td>
+              <td>
+                You are now running Bitnami <a href="https://bitnami.com/stack/meanstack?utm_source=bitnami&utm_medium=cloudimage&utm_campaign=google">MEAN</a> 3.4.3-0 in the Cloud.
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div id="dashboard-right-area">
+          <div class="dashboard-button-big launch-button launch-button-blue"><a id="bitnami-link" href="https://docs.bitnami.com/google/infrastructure/mean/?utm_source=bitnami&utm_medium=cloudimage&utm_campaign=google">Quick Start Guide</a></div>
+          <div class="dashboard-button-big launch-button"><a href="https://docs.bitnami.com/google/components/rockmongo/?utm_source=bitnami&utm_medium=cloudimage&utm_campaign=google">Access RockMongo</a></div>
+          <div class="dashboard-button-big launch-button"><a href="https://docs.bitnami.com/google/infrastructure/mean/?utm_source=bitnami&utm_medium=cloudimage&utm_campaign=google">Bitnami Documentation</a></div>
+          <div class="dashboard-button-big launch-button"><a href="https://community.bitnami.com?utm_source=bitnami&utm_medium=cloudimage&utm_campaign=google">Bitnami Forums</a></div>
+        </div>
+        <br/>
+      <div class="clear"></div>
+      
+ 
+      </div>
+    </div>
+  </body>
+</html>
